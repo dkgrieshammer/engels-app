d7059c6d3bd294bb8c782b30062af410de9ab849
David Grieshammer <dkgrieshammer@gmail.com>
2020-08-27 10:48:01 +0200

    added imageviewer and pdf viewer

--- src/components/Imageviewer/Imageviewer.js
+++ src/components/Imageviewer/Imageviewer.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Imageviewer.module.scss'
+import {MapInteractionCSS} from 'react-map-interaction'
 
 export const Imageviewer = ({image, callback}) => {
+
+  const [state, setState] = useState({
+    value: {
+      scale: 1,
+      translation: {x:0,y:0}
+    },
+    item: undefined,
+    isLoaded: false
+  })
+
+  useEffect(() => {
+    fetch(image)
+      .then(
+        (res) => {
+        setState({isLoaded:true})
+        console.log(res)
+      })
+    let scrollTop = document.getElementById('textPDF').contentWindow.document.body.scrollTop;
+    console.log(scrollTop)
+  },[image])
+
+  const onChange = (newState) => {
+    console.log(newState)
+    // newState.value.translation.x = 0
+    setState(newState)
+  }
+  
+
   return (
     <div className={styles.container}>
-      viewer
+      <div>
+        <iframe id='textPDF' className={styles.pdf} src={image}></iframe>
+      </div>
+      {state.isLoaded && <div className={styles.test}>PETER</div>}
     </div>
   )
 }
+
+
+//https://chost20.zim.uni-wuppertal.de/api/facsimile/FE_06.1_22051791.pdfo
--- src/components/Imageviewer/Imageviewer.stories.js
+++ src/components/Imageviewer/Imageviewer.stories.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Imageviewer } from './Imageviewer';
+import Faksimile from './../../static/images/faksimile.png'
 
 export default {
-  title: 'Imageviewer'
+  title: 'Letter/Imageviewer'
 }
 
-export const Default = () => <Imageviewer />
+export const Default = () => <Imageviewer image={'https://chost20.zim.uni-wuppertal.de/api/facsimile/FE_06.1_22051791.pdf'} />
