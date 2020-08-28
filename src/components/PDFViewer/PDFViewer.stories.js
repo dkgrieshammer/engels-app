4b2a2b6ad433ac011b9c495525967b066cbc617c
David Grieshammer <dkgrieshammer@gmail.com>
2020-08-28 11:49:32 +0200

    removing react-pdf-js

--- src/components/PDFViewer/PDFViewer.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { PDFViewer } from './PDFViewer';
-
-export default {
-  title: 'Letter/PDFViewer'
-}
-
-export const Default = () => <PDFViewer file='https://chost20.zim.uni-wuppertal.de/api/facsimile/FE_06.1_22051791.pdf' />
