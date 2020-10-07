7e30c5b69f11c0720cfc053a5002b0558f657868
Tim Helfensdörfer <tim.helfensdoerfer@uni-wuppertal.de>
2020-10-07 18:14:33 +0200

    Implemented a basic letter view

--- src/components/Letterviews/Normalview/Normalview.stories.js
+++ src/components/Letterviews/Normalview/Normalview.stories.js
@@ -2,13 +2,10 @@ import React from 'react';
 import { Normalview } from './Normalview';
 import response from '../Letter.data';
 
-
 export default {
   title: 'Letter/Letterviews'
 }
 
 const data = response
-console.log("data is ", data.data.text.body.div)
-
 
 export const Default = () => <Normalview data={data} />
